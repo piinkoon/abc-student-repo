64ee6c3d77dfeeddb0e4c69f723af3b9f7a3a41b
Oona Pecson <op531@nyu.edu>
2021-09-28 16:09:15 -0400

    update

---
+++ projects/projectA/6am/script.js
@@ -0,0 +1,8 @@
+// let occurEvery = 120; 
+// setInterval(()=>{
+//     let dt = new Date();
+//     let minutes = dt.getMinutes() + (60*dt.getHours())
+//     if(minutes%occurEvery){
+//         window.open("../8am/index.html","_self");
+//     }
+// }, 40000)
