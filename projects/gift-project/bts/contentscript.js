e6bd7422055a0611e2af333218e3529c40361186
Oona Pecson <op531@nyu.edu>
2021-10-20 19:42:23 -0400

    finished gift extension

--- projects/gift-project/bts/contentscript.js
+++ projects/gift-project/bts/contentscript.js
@@ -1,1 +1,12 @@
+(function() {
 
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'You should open a new tab!';
+	document.body.appendChild(div);
+
+	alert("Hey! Stop what you're doing & look at the top right of the page! 👀");
+
+})();
