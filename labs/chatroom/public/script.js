05c79b23597959357f89b01f6df0b6bb48b6987b
Oona Pecson <op531@nyu.edu>
2021-11-14 00:50:52 -0500

    chatroom!

---
+++ labs/chatroom/public/script.js
@@ -0,0 +1,42 @@
+console.log("hi");
+
+let socket = io();
+let namebox = document.getElementById("name");
+let chatbox = document.getElementById("chat");
+let messagebox = document.getElementById("message");
+let sendbutton = document.getElementById("send");
+
+sendbutton.addEventListener("click", () => {
+  console.log("clicked!");
+  let name = namebox.value.trim();
+  if(name == ""){
+    name = "anonymous";
+    namebox.value = "";
+  }
+  let message = messagebox.value.trim();
+  console.log(message);
+  if(message != ""){
+    let data = {name: name, message: message}
+    socket.emit('message', data);
+    console.log(data);
+  }
+  messagebox.value = "";
+})
+
+socket.on("incoming", (data) => {
+  console.log(data);
+  let name = data.name;
+  let message = data.message;
+  let li = document.createElement("li");
+  let p = document.createElement("p");
+  p.innerHTML = "<span class='sender'>"+name+":</span> " + message;
+  li.appendChild(p);
+  chatbox.appendChild(li);
+  chatbox.scrollTop = chatbox.scrollHeight;
+})
+
+messagebox.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    sendbutton.click();
+  }
+});
---
+++ labs/chatroom/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("incoming", data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
